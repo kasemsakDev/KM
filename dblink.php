c3af3b8509946b524e95139f79550e597816989f
KasemsakDev <namekasemsak17@gmail.com>
2021-02-22 01:14:01 +0700

    update DB

--- dblink.php
+++ dblink.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = @mysqli_connect("localhost","root","","pmqa") or die(mysqli_connect_error());
+$link = @mysqli_connect("us-cdbr-east-03.cleardb.com","b054da527cc859","7ac4899e","heroku_3994a86bca8479f") or die(mysqli_connect_error());
 
 $link->set_charset("utf8");
 
